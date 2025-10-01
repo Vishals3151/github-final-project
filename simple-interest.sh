3a813cd205902755fd5a87aa231ea3f0a14a19e3
Vishal Gaikwad <vishalsureshgaikwad115@gmail.com>
2025-10-01 09:12:43 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,80 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+
+1. #!/bin/bash
+
+2.
+
+This script calculates simple interest given principal,
+
+3 annual rate of interest and time period in years.
+
+5. #Do not use this in production. Sample purpose only.
+
+6
+
+#Author: Upkar Lidder (18)
+
+#Additional Authors:
+
+9 #cyour Github username>
+
+10.
+
+11. #Input:
+
+12. #p. principal amount
+
+13. t.me period in years
+
+14. #r, annual rate of interest
+
+15.
+
+16. #Output:
+
+17 #simple interest pter
+
+18
+
+19. echo "Enter the principal:"
+
+20. read p
+
+21. echo "Enter rate of interest per year:
+
+22. read r 23. echo "Enter time period in years:"
+
+24 read t
+
+25.
+
+26. sexpr Sp St Sr/100
+
+27. echo "The simple
